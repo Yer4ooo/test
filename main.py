4462549774dd788ed91615f350ae8868b72bc75d
Yer4ooo <yernar.ainukatov@nu.edu.kz>
2023-08-25 20:37:56 +0600

    I am testing my skills

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-# This is a sample Python script.
+s = 'hello'
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+i = s.__iter__()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-print("hello world")
-print("Today I started my way 04.07.2023!!!")
+for x in range(0, 5):
+    print(next(i).title())
